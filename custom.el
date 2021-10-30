107da20e116d57d904b23d0a6f66016812faf4db
zawy <46110056+zoeyalex@users.noreply.github.com>
2021-10-30 23:58:54 +0200

    update

--- custom.el
+++ custom.el
@@ -3,28 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "839d3af9aef7ff18597d852b829e39120f0b5ad16a6ae99eb74cc348fc6d46e6" default))
- '(global-emojify-mode t)
- '(hl-todo-keyword-faces
-   '(("TODO" . "#dc752f")
-     ("NEXT" . "#dc752f")
-     ("THEM" . "#2d9574")
-     ("PROG" . "#4f97d7")
-     ("OKAY" . "#4f97d7")
-     ("DONT" . "#f2241f")
-     ("FAIL" . "#f2241f")
-     ("DONE" . "#86dc2f")
-     ("NOTE" . "#b1951d")
-     ("KLUDGE" . "#b1951d")
-     ("HACK" . "#b1951d")
-     ("TEMP" . "#b1951d")
-     ("FIXME" . "#dc752f")
-     ("XXX+" . "#dc752f")
-     ("\\?\\?\\?+" . "#dc752f")))
  '(package-selected-packages
-   '(emojify inkpot-theme spacemacs-theme xref which-key use-package undo-tree rainbow-mode rainbow-delimiters projectile ivy-rich hydra helpful general ewal evil-collection elcord dracula-theme doom-modeline counsel consult-lsp autothemer))
- '(pdf-view-midnight-colors '("#b2b2b2" . "#292b2e")))
+   '(pyvenv latex-preview-pane latex-math-preview all-the-icons xref which-key use-package undo-tree spacemacs-theme rainbow-mode rainbow-delimiters projectile page-break-lines ivy-rich inkpot-theme hydra helpful general ewal evil-collection emojify elcord dracula-theme doom-modeline dashboard counsel consult-lsp autothemer)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
